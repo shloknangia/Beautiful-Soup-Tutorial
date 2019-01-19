5ba5dff9f3c665563f207251bdf2506841e1e677
shlok nangia <shloknangia@gmail.com>
2019-01-19 12:51:17 +0530

    added white_house_example.py file

---
+++ white_house_example.py
@@ -0,0 +1,13 @@
+import requests
+from bs4 import BeautifulSoup
+
+result = requests.get("https://www.whitehouse.gov/briefings-statements/")
+src = result.content
+soup = BeautifulSoup(src, 'lxml')
+
+urls = []
+for h2_tag in soup.find_all("h2"):
+    a_tag = h2_tag.find('a').attrs['href']
+    urls.append(a_tag)
+
+print(urls)
